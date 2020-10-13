44de41c048b53d1a90f3940aa666dac564bae2df
YouTubePlays <allplayslive@gmail.com>
2020-10-13 00:43:08 -0700

    Tutorial UI

---
+++ TutorialUI/precache-manifest.e9d974f5285ba697f42f2fde26b8229e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d9dc4707fab687e68fb72530f370535",
+    "url": "./index.html"
+  },
+  {
+    "revision": "240af9e31830ccecff30",
+    "url": "./static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "f3f8861c68b244e3f2b7",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "240af9e31830ccecff30",
+    "url": "./static/js/2.4a7557c6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.4a7557c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3f8861c68b244e3f2b7",
+    "url": "./static/js/main.8e6ac8e0.chunk.js"
+  },
+  {
+    "revision": "a919de0e72d9c3d6c2e1",
+    "url": "./static/js/runtime-main.65f28705.js"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "./static/media/getFetch.013f0b90.cjs"
+  }
+]);
